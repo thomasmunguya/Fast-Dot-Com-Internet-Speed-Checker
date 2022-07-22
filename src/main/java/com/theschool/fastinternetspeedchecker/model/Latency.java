d5468271edc1687b11aaed1ec4761ef23c07fc45
Thomas Munguya <thomas@thezig.io>
2022-07-22 18:02:56 +0200

    Added Latency record.

---
+++ src/main/java/com/theschool/fastinternetspeedchecker/model/Latency.java
@@ -0,0 +1,9 @@
+package com.theschool.fastinternetspeedchecker.model;
+
+/**
+ * This record encapsulates the internet latency information as it appears on the <a>fast.com</a> website.
+ *
+ * @author Thomas Munguya
+ */
+public record Latency (String loaded, String unloaded) {
+}
